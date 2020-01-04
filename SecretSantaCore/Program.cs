11c2b3db7437932ba00b5e3f4a65eb3e5b749b59
FindingNamo <davidminhnamngo@gmail.com>
2020-01-03 20:17:54 -0800

    Adding dotnet core version and updated package

---
+++ SecretSantaCore/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace SecretSanta
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var picker = new SecretSantaPicker("participants.json");
+            var mailer = new SecretSantaMailer("mailSettings.json");
+
+            var results = picker.DrawNames();
+
+            // Ensure the results directory is available.
+            if (!Directory.Exists("results"))
+                Directory.CreateDirectory("results");
+
+            bool allEmailsValid = true;
+            if (results != null)
+            {
+                Console.WriteLine("Found a valid match for {0} participants after {1} attempt(s).", picker.Participants.Count, picker.Attempts);
+                Console.WriteLine("");
+                foreach (var kvp in results)
+                {
+                    Console.WriteLine("Recording result for {0}....", kvp.Key.Name);
+                    mailer.WriteFile(kvp.Key, kvp.Value);
+                    if (string.IsNullOrEmpty(kvp.Key.Email))
+                    {
+                        allEmailsValid = false;
+                        ConsoleColor consoleColor = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("{0} has no email address.", kvp.Key.Name);
+                        Console.ForegroundColor = consoleColor;
+                    }
+                }
+            }
+
+            if (allEmailsValid)
+            {
+                Console.WriteLine("Press Enter to send mails.");
+                Console.ReadLine();
+                foreach (var kvp in results)
+                {
+                    int sendMailAttempts = 0;
+                    while (sendMailAttempts < 3)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Sending Mail to {0} ({1}).", kvp.Key.Name, kvp.Key.Email);
+                            mailer.SendMail(kvp.Key, kvp.Value);
+                            break;
+                        }
+                        catch
+                        {
+                            ConsoleColor consoleColor = Console.ForegroundColor;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Failed to send mail to {0} ({1})", kvp.Key.Name, kvp.Key.Email);
+                            Console.ForegroundColor = consoleColor;
+                            sendMailAttempts++;
+                            Thread.Sleep(1000);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("[Press any key to exit...]");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SecretSantaCore/SecretSantaMailSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta
+{
+    [Serializable]
+    public class SecretSantaMailSettings
+    {
+        /// <summary>
+        /// The gmail account to send mail from.
+        /// </summary>
+        public string SantasEmail;
+
+        /// <summary>
+        /// The password of the above account.
+        /// </summary>
+        public string SantasPassword;
+
+        /// <summary>
+        /// The message to be in the subject of the announcement mail.
+        /// {0} -- Gifter's Name
+        /// {1} -- Recipient's Name
+        /// </summary>
+        public string SantasSubject;
+
+        /// <summary>
+        /// The message to be in the body of the announcement mail.
+        /// HTML is supported.
+        /// {0} -- Gifter's Name
+        /// {1} -- Recipient's Name
+        /// </summary>
+        public string SantasMessage;
+    }
+}
